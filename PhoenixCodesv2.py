ddda6898cabdec9e77f4d4ea6208afb0062be297
EC2 Default User <ec2-user@ip-172-31-15-132.us-east-2.compute.internal>
2024-06-24 20:54:56 +0000

    6 threads is too many, stuck with 4 I guess.

--- PhoenixCodesv2.py
+++ PhoenixCodesv2.py
@@ -194,7 +194,7 @@ current_time = datetime.now()
 time_stamp = f"{current_time.year}-{current_time.month}-{current_time.day}_{current_time.hour}-{current_time.minute}-{current_time.second}"
 total_length = len(houses)
 file_name = f"data/PhoeAddrResults-{time_stamp}.json"
-run_threads(6, houses, lock, write_lock, prog_bar)
+run_threads(4, houses, lock, write_lock, prog_bar)
 
 # Compute total time...
 end_time = time.time()
