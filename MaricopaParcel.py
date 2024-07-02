01d00e25d0c754c2dce17a7cf415f8833ccfa4f6
AndrewErting <andrew.erting@gmail.com>
2024-07-02 14:54:38 -0700

    Removing unused import

--- MaricopaParcel.py
+++ MaricopaParcel.py
@@ -13,7 +13,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
 
 
 def get_config():
